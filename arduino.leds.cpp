ae480d420936d7af300d2b7f308d28001123181b
NiX0n <BiggoronSword+GitHub@gmail.com>
2019-03-21 21:50:23 -0400

    Added: FastLED Hello World

--- arduino.leds.cpp
+++ arduino.leds.cpp
@@ -1,1 +1,18 @@
+#include "FastLED.h"
 
+CRGB leds[9];
+
+void setup()
+{
+	FastLED.addLeds<NEOPIXEL, 6>(leds, 9);
+	pinMode(LED_BUILTIN, OUTPUT);
+	Serial.begin(9600);
+}
+
+void loop()
+{
+	leds[0] = CRGB::White;
+	leds[1] = CRGB::Red;
+	FastLED.show();
+	delay(1000);
+}

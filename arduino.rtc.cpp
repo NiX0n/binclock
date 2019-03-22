391fc4011a5b581602bc7c3973c2e3ab9f5f2efa
NiX0n <BiggoronSword+GitHub@gmail.com>
2019-03-22 13:48:55 -0400

    Created: arduino.rtc.cpp

---
+++ arduino.rtc.cpp
@@ -0,0 +1,10 @@
+// References:
+// https://github.com/sparkfun/SparkFun_DS3234_RTC_Arduino_Library
+
+#define DS13074_CS_PIN 11
+#define DS13074_MOSI_PIN 10
+#define DS13074_MISO_PIN 9
+#define DS13074_CLK_PIN 8
+
+#include <SPI.h>
+#include "SparkFunDS3234RTC.h"
